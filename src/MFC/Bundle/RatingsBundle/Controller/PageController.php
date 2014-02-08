023e9e8cff2dc487e0a9b50d271eaa3cee4e025d
Parth Sarin <parthsarin@gmail.com>
2014-02-07 21:44:58 -0600

    Add Bootstrap and Change the base template

---
+++ src/MFC/Bundle/RatingsBundle/Controller/PageController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace MFC\Bundle\RatingsBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+
+class PageController extends Controller
+{
+	/**
+	 * @Route("/", name="page_index")
+	 * @Method("GET")
+	 * @Template
+	 */
+	public function indexAction()
+	{
+		
+	}
+}
