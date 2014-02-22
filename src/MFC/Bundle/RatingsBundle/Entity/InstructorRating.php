8d6dcb05df3709d0ee22cb2d83bb49be4cb3d723
Parth Sarin <parthsarin@gmail.com>
2014-02-21 18:20:14 -0600

    Multiple Changes
    + Added InstructorRating Entity
    * Updated Styles for Form Errors
    * Added Javascript to Student Form Template to Display Proper Divs on Load

---
+++ src/MFC/Bundle/RatingsBundle/Entity/InstructorRating.php
@@ -0,0 +1,277 @@
+<?php
+
+namespace MFC\Bundle\RatingsBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\ExecutionContextInterface;
+
+use MFC\Bundle\RatingsBundle\Entity\Maplet;
+
+/**
+ * InstructorRating
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class InstructorRating
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="methodUsed", type="string", length=255)
+     */
+    private $methodUsed;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="methodUsedOther", type="text")
+     */
+    private $methodUsedOther;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="skillsDeveloped", type="string", length=255)
+     */
+    private $skillsDeveloped;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="skillsDevelopedOther", type="text")
+     */
+    private $skillsDevelopedOther;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="rating", type="string", length=255)
+     */
+    private $rating;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="comments", type="text")
+     */
+    private $comments;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="created", type="datetime")
+     */
+    private $created;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Maplet")
+     * @ORM\JoinColumn(name="maplet_id", referencedColumnName="id")
+     */
+    private $maplet;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set methodUsed
+     *
+     * @param string $methodUsed
+     * @return InstructorRating
+     */
+    public function setMethodUsed($methodUsed)
+    {
+        $this->methodUsed = $methodUsed;
+
+        return $this;
+    }
+
+    /**
+     * Get methodUsed
+     *
+     * @return string 
+     */
+    public function getMethodUsed()
+    {
+        return $this->methodUsed;
+    }
+
+    /**
+     * Set methodUsedOther
+     *
+     * @param string $methodUsedOther
+     * @return InstructorRating
+     */
+    public function setMethodUsedOther($methodUsedOther)
+    {
+        $this->methodUsedOther = $methodUsedOther;
+
+        return $this;
+    }
+
+    /**
+     * Get methodUsedOther
+     *
+     * @return string 
+     */
+    public function getMethodUsedOther()
+    {
+        return $this->methodUsedOther;
+    }
+
+    /**
+     * Set skillsDeveloped
+     *
+     * @param string $skillsDeveloped
+     * @return InstructorRating
+     */
+    public function setSkillsDeveloped($skillsDeveloped)
+    {
+        $this->skillsDeveloped = $skillsDeveloped;
+
+        return $this;
+    }
+
+    /**
+     * Get skillsDeveloped
+     *
+     * @return string 
+     */
+    public function getSkillsDeveloped()
+    {
+        return $this->skillsDeveloped;
+    }
+
+    /**
+     * Set skillsDevelopedOther
+     *
+     * @param string $skillsDevelopedOther
+     * @return InstructorRating
+     */
+    public function setSkillsDevelopedOther($skillsDevelopedOther)
+    {
+        $this->skillsDevelopedOther = $skillsDevelopedOther;
+
+        return $this;
+    }
+
+    /**
+     * Get skillsDevelopedOther
+     *
+     * @return string 
+     */
+    public function getSkillsDevelopedOther()
+    {
+        return $this->skillsDevelopedOther;
+    }
+
+    /**
+     * Set rating
+     *
+     * @param string $rating
+     * @return InstructorRating
+     */
+    public function setRating($rating)
+    {
+        $this->rating = $rating;
+
+        return $this;
+    }
+
+    /**
+     * Get rating
+     *
+     * @return string 
+     */
+    public function getRating()
+    {
+        return $this->rating;
+    }
+
+    /**
+     * Set comments
+     *
+     * @param string $comments
+     * @return InstructorRating
+     */
+    public function setComments($comments)
+    {
+        $this->comments = $comments;
+
+        return $this;
+    }
+
+    /**
+     * Get comments
+     *
+     * @return string 
+     */
+    public function getComments()
+    {
+        return $this->comments;
+    }
+
+    /**
+     * Set created
+     *
+     * @param \DateTime $created
+     * @return InstructorRating
+     */
+    public function setCreated($created)
+    {
+        $this->created = $created;
+
+        return $this;
+    }
+
+    /**
+     * Get created
+     *
+     * @return \DateTime 
+     */
+    public function getCreated()
+    {
+        return $this->created;
+    }
+
+    /**
+     * Set maplet
+     *
+     * @param \MFC\Bundle\RatingsBundle\Entity\Maplet $maplet
+     * @return InstructorRating
+     */
+    public function setMaplet(\MFC\Bundle\RatingsBundle\Entity\Maplet $maplet = null)
+    {
+        $this->maplet = $maplet;
+
+        return $this;
+    }
+
+    /**
+     * Get maplet
+     *
+     * @return \MFC\Bundle\RatingsBundle\Entity\Maplet 
+     */
+    public function getMaplet()
+    {
+        return $this->maplet;
+    }
+}
